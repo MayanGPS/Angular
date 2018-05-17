8fd238c861c9c7e28c92783bc6d7843ed289bf75
Benedeck Chavez <benedeck@hotmail.com>
2018-05-17 16:11:55 -0500

    penultimo commit

--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InterruptorComponent } from './interruptor/interruptor.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InterruptorComponent],
+  exports: [InterruptorComponent]
 })
 export class UtilsModule { }
